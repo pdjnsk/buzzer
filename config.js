63246fd4fd37e1aa07272e05882379fcc399d7b5
pdjnsk <80638504+pdjnsk@users.noreply.github.com>
2021-03-24 12:13:24 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAh6rRrImpSaQj2IEUQ5r14M1H_L_DU-ck",
+    authDomain: "buzzer-app-883ee.firebaseapp.com",
+    databaseURL: "https://buzzer-app-883ee-default-rtdb.firebaseio.com",
+   projectId: "buzzer-app-883ee",
+    storageBucket: "buzzer-app-883ee.appspot.com",
+    messagingSenderId: "956698333525",
+    appId: "1:956698333525:web:ad6793b1b6e8d37ced9fdc"
+  };
+
+
+// Initialize Firebase
+if(!firebase.apps.length){
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default  firebase.database()
